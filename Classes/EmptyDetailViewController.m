24e3319a4d5fb351cbee964a47c3e894b1096406
Chad Gibbons <dcgibbons@gmail.com>
2011-03-07 16:05:49 -0600

    refactor root view controller to use idiomatic xib file names

--- Classes/EmptyDetailViewController.m
+++ Classes/EmptyDetailViewController.m
@@ -11,41 +11,9 @@
 
 @implementation EmptyDetailViewController
 
-// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
-- (void)viewDidLoad 
-{
-    [super viewDidLoad];
-    
-    self.title = @"Fitness Nut";
-    self.navigationItem.title = @"Fitness Nut";
-}
-
-
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation 
 {
     return YES;
 }
-
-- (void)didReceiveMemoryWarning 
-{
-    // Releases the view if it doesn't have a superview.
-    [super didReceiveMemoryWarning];
-    
-    // Release any cached data, images, etc. that aren't in use.
-}
-
-- (void)viewDidUnload 
-{
-    [super viewDidUnload];
-    // Release any retained subviews of the main view.
-    // e.g. self.myOutlet = nil;
-}
-
-
-- (void)dealloc 
-{
-    [super dealloc];
-}
-
 
 @end
